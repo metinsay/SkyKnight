001de08d9ee4a3f95b7af08e9fc18b476cd9ed22
tysonzero <danielspencersmith@gmail.com>
2018-04-08 20:53:25 -0400

    Add obstacles

--- External/Wingsuit.hs
+++ External/Wingsuit.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 import Control.Lens
+import Data.Bool (bool)
+import Data.Foldable (fold)
+import Data.Monoid ((<>))
 import Graphics.Gloss.Interface.Pure.Game
 
 gravity :: Float
@@ -15,11 +18,17 @@ lift = 2
 rotVel :: Float
 rotVel = 3
 
+initialBlocks =
+    [ ( (-650, -400), (-350, -50) )
+    , ( (-650, -400), (150, -200) )
+    ]
+
 data State = State
     { _position :: Point
     , _velocity :: Point
     , _rotation :: Float
     , _keys :: (Bool, Bool)
+    , _blocks :: [(Point, Point)]
     } deriving Show
 makeLenses ''State
 
@@ -36,15 +45,14 @@ framerate :: Int
 framerate = 60
 
 initial :: State
-initial = State (-400, 300) (0, 0) 1 (False, False)
+initial = State (-600, 350) (0, 0) 0 (False, False) initialBlocks
 
 render :: State -> Picture
-render (State (x, y) _ r _) = polygon
-    [ (x + 20 * cos r - 5 * sin r, y + 20 * sin r + 5 * cos r)
-    , (x + 20 * cos r + 5 * sin r, y + 20 * sin r - 5 * cos r)
-    , (x - 20 * cos r + 5 * sin r, y - 20 * sin r - 5 * cos r)
-    , (x - 20 * cos r - 5 * sin r, y - 20 * sin r + 5 * cos r)
-    ]
+render s = renderPlayer <> fold renderBlocks
+  where
+    renderPlayer = polygon $ playerPoints s
+    renderBlocks = s ^. blocks <&> \((x1, y1), (x2, y2)) ->
+        polygon [(x1, y1), (x1, y2), (x2, y2), (x2, y1)]
 
 handle :: Event -> State -> State
 handle (EventKey (Char 'a') Down _ _) s = s & keys . _1 .~ True
@@ -54,16 +62,33 @@ handle (EventKey (Char 'd') Up _ _) s = s & keys . _2 .~ False
 handle _ s = s
 
 step :: Float -> State -> State
-step t s = s
-    & velocity +~ (0, - gravity * t)
-    & velocity *~ ((1 - drag) ** t) .* 1
-    & velocity -~ lift * t * uncurry (+) (norm * s ^. velocity) .* norm
-    & position +~ t .* s ^. velocity
-    & rotation +~ rotVel * t * fromIntegral (fromEnum $ s ^. keys . _1)
-    & rotation -~ rotVel * t * fromIntegral (fromEnum $ s ^. keys . _2)
+step t s = s & updateVelocity & updatePosition & updateRotation & checkCollision
   where
     norm = (- sin (s ^. rotation), cos (s ^. rotation))
+    updateVelocity s = s
+        & velocity +~ (0, - gravity * t)
+        & velocity *~ ((1 - drag) ** t) .* 1
+        & velocity -~ lift * t * uncurry (+) (norm * s ^. velocity) .* norm
+    updatePosition s = s & position +~ t .* s ^. velocity
+    updateRotation s = s
+        & rotation +~ rotVel * t * fromIntegral (fromEnum $ s ^. keys . _1)
+        & rotation -~ rotVel * t * fromIntegral (fromEnum $ s ^. keys . _2)
+    checkCollision s = bool s initial . or $ inBlock <$> playerPoints s <*> s ^. blocks
+
+playerPoints :: State -> [Point]
+playerPoints s =
+    [ (x + 20 * cos r - 5 * sin r, y + 20 * sin r + 5 * cos r)
+    , (x + 20 * cos r + 5 * sin r, y + 20 * sin r - 5 * cos r)
+    , (x - 20 * cos r + 5 * sin r, y - 20 * sin r - 5 * cos r)
+    , (x - 20 * cos r - 5 * sin r, y - 20 * sin r + 5 * cos r)
+    ]
+  where
+    (x, y) = s ^. position
+    r = s ^. rotation
 
 (.*) :: Float -> Point -> Point
 m .* (x, y) = (m * x, m * y)
 infixl 7 .*
+
+inBlock :: Point -> (Point, Point) -> Bool
+inBlock (x, y) ((x1, y1), (x2, y2)) = x > x1 && x < x2 && y > y1 && y < y2
