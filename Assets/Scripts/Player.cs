ae2dd78c689c9fa642ba156229f168df7de677c8
Aashish Welling <aashishwelling@gmail.com>
2018-04-15 23:48:15 -0400

    start of port to unity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,32 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
-	public float speed = 1.0f;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Vector3 pos = transform.position;
+public class Player : MonoBehaviour
+{
+    public double gravity = 200f;
+    public double drag = 0.01f;
+    // Use this for initialization
 
-		if (Input.GetKey ("w")) {
-			pos.y += speed * Time.deltaTime;
-		}
-		if (Input.GetKey ("s")) {
-			pos.y -= speed * Time.deltaTime;
-		}
-		if (Input.GetKey ("d")) {
-			pos.x += speed * Time.deltaTime;
-		}
-		if (Input.GetKey ("a")) {
-			pos.x -= speed * Time.deltaTime;
-		}
+    double GetLift(double v, double x)
+    {
+        return 0.002 * Math.Pow(v, 2) * Math.Pow(x, 0.3);
+    }
+    void Start()
+    {
 
+    }
 
-		transform.position = pos;
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 pos = transform.position;
+        Debug.DrawRay(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), Color.red);
+
+        transform.eulerAngles = new Vector3(0, 0, 200 - Vector2.SignedAngle(mouse, pos));
+        //transform.rotation = Quaternion.LookRotation((mouse - pos).normalized);
+    }
 }
