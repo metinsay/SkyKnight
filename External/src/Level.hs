d7bc1a7616736382ed0bfeae45bcaebaf90f8dc3
tysonzero <danielspencersmith@gmail.com>
2018-04-19 16:52:32 -0400

    Split Level related code into Level module

---
+++ External/src/Level.hs
@@ -0,0 +1,22 @@
+module Level
+    ( Level(Level)
+    , level
+    ) where
+
+import Base
+import Block
+
+data Level = Level Point [Block]
+
+level :: Level
+level = Level initialPlayer initialBlocks
+
+initialPlayer :: Point
+initialPlayer = (50, 11500)
+
+initialBlocks :: [Block]
+initialBlocks = [ ((-1000, -1000), (0, 12000)) ]
+    <|> (\(x, y) -> ((x, y - 10000), (x + 50, y))) <$> zip [0, 50 ..] heights
+  where
+    heights = [ (6000 + cos (x / 50) * 4000) * 0.9985 ** x | x <- [0 .. 2000] ]
+    -- heights = [ 6000 * (sin (x / 50) / (2 ** ((x / 50 - pi / 2) / pi))) | x <- [2 * 50 .. 1500] ]
--- External/src/Main.hs
+++ External/src/Main.hs
@@ -6,18 +6,9 @@ import Base
 import Block (Block)
 import qualified Block as B
 import qualified Hud as H
+import Level (Level(Level), level)
 import Player (Player)
 import qualified Player as P
-
-initialPlayer :: Point
-initialPlayer = (50, 11500)
-
-initialBlocks :: [Block]
-initialBlocks = [ ((-1000, -1000), (0, 12000)) ]
-    <|> (\(x, y) -> ((x, y - 10000), (x + 50, y))) <$> zip [0, 50 ..] heights
-  where
-    heights = [ (6000 + cos (x / 50) * 4000) * 0.9985 ** x | x <- [0 .. 2000] ]
-    -- heights = [ 6000 * (sin (x / 50) / (2 ** ((x / 50 - pi / 2) / pi))) | x <- [2 * 50 .. 1500] ]
 
 data State = State
     { _player :: Player
@@ -30,7 +21,8 @@ main :: IO ()
 main = play FullScreen (makeColor 1 1 1 1) 60 initial render handle step
 
 initial :: State
-initial = State (P.create initialPlayer) initialBlocks
+initial = case level of
+    Level pPos bs -> State (P.create pPos) bs
 
 render :: State -> Picture
 render s = uncurry translate (- P.getPosition (s ^. player)) (P.render $ s ^. player)
@@ -43,7 +35,8 @@ handle e s = s & player %~ P.handle e
 step :: Float -> State -> State
 step t = checkCollision . (player %~ P.step t)
   where
-    checkCollision s = s & player %~ \p -> bool p (P.reset initialPlayer p) . or
+    Level pPos _ = level
+    checkCollision s = s & player %~ \p -> bool p (P.reset pPos p) . or
         $ inBlock <$> P.getPoints p <*> s ^. blocks
 
 inBlock :: Point -> (Point, Point) -> Bool
