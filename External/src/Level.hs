8a31b570a1fa8893af0b0b52cd273cba5c5d28dc
tysonzero <danielspencersmith@gmail.com>
2018-04-20 15:33:17 -0400

    Refactor

--- External/src/Level.hs
+++ External/src/Level.hs
@@ -18,13 +18,7 @@ data Level = Level
 makeLenses ''Level
 
 level :: Level
-level = Level initialPlayer initialBlocks
-
-initialPlayer :: Point
-initialPlayer = (50, 11500)
-
-initialBlocks :: [Block]
-initialBlocks = [ ((-1000, -1000), (0, 12000)) ]
+level = Level (50, 11500) $ [ ((-1000, -1000), (0, 12000)) ]
     <|> (\(x, y) -> ((x, y - 10000), (x + 50, y))) <$> zip [0, 50 ..] heights
   where
     heights = [ (6000 + cos (x / 50) * 4000) * 0.9985 ** x | x <- [0 .. 2000] ]
