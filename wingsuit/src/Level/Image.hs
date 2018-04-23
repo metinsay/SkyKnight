9b0630182e357977c93342cd0c37d65ee47395bc
tysonzero <danielspencersmith@gmail.com>
2018-04-23 17:04:41 -0400

    Code cleanup

--- wingsuit/src/Level/Image.hs
+++ wingsuit/src/Level/Image.hs
@@ -3,28 +3,34 @@ module Level.Image
     ( loadLevel
     ) where
 
-import Base
-import Level (Level (Level))
-import Codec.Picture (convertRGBA8, readImage, pixelAt, PixelRGBA8 (PixelRGBA8), imageWidth, imageHeight)
-import Graphics.Gloss.Juicy (fromDynamicImage)
+import Codec.Picture
+    ( convertRGBA8, readImage, pixelAt, PixelRGBA8(PixelRGBA8), imageWidth, imageHeight )
 import Data.Maybe (fromJust)
+import Graphics.Gloss.Juicy (fromDynamicImage)
+
+import Base
+import Level (Level(Level), _getIsTerrain, _getTerrain, _isFinish, _start)
 
 isTerrain :: FilePath -> IO (Point -> Bool)
 isTerrain path = readImage path >>= \case
-                    Left err -> error err
-                    Right img -> 
-                        let conv = convertRGBA8 img
-                        in 
-                            return (\(x, y) -> pixelAt
-                            conv
-                            (imageWidth conv `div` 2 + round x)
-                            (imageHeight conv `div` 2 - round y) /= PixelRGBA8 0 0 0 0)
+    Left err -> error err
+    Right img -> pure $ \(x, y) -> let
+        conv = convertRGBA8 img
+        pixel = pixelAt
+            conv
+            (imageWidth conv `div` 2 + round x)
+            (imageHeight conv `div` 2 - round y)
+        in pixel /= PixelRGBA8 0 0 0 0
 
 terrain :: FilePath -> IO Picture
 terrain path = readImage path >>= \case
-                    Left err -> error err
-                    Right img -> return $ fromJust $ fromDynamicImage img
+    Left err -> error err
+    Right img -> return $ fromJust $ fromDynamicImage img
 
---        Level folder-> start -> isFinish        -> returns level
 loadLevel :: FilePath -> Point -> (Point -> Bool) -> Level
-loadLevel path start isFinish = Level start isFinish (isTerrain $ path ++ "/collision.bmp") (terrain $ path ++ "/art.bmp")
+loadLevel path start isFinish = Level
+    { _start = start
+    , _isFinish = isFinish
+    , _getIsTerrain = isTerrain $ path ++ "/collision.bmp"
+    , _getTerrain = terrain $ path ++ "/art.bmp"
+    }
