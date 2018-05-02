2c6261f76a07da8e68688d74637cb6b843837a2f
Drew Beller <drew@1627-laptop.local>
2018-05-01 19:08:16 -0700

    playable level 2

--- wingsuit/src/Levels/Image.hs
+++ wingsuit/src/Levels/Image.hs
@@ -17,7 +17,7 @@ level1 :: Level
 level1 = loadLevel "levels/level1" (-19750, 4750) ((19900, -5000), (20000, 5000)) 100
 
 level2 :: Level
-level2 = loadLevel "levels/level2" (-1400, 400) ((0, 0), (0, 0)) 100
+level2 = loadLevel "levels/level2" (-19500, 4750) ((19900, -5000), (20000, 5000)) 100
 
 level3 :: Level
 level3 = loadLevel "levels/level3" (-1400, 400) ((0, 0), (0, 0)) 100
