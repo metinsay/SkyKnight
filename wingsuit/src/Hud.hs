b0f31fc692ce4c8f79c750e1558a04954a70ada2
tysonzero <danielspencersmith@gmail.com>
2018-05-11 20:22:43 -0400

    Add speedometer

--- wingsuit/src/Hud.hs
+++ wingsuit/src/Hud.hs
@@ -12,18 +12,30 @@ import qualified World as W
 data Hud = Hud
     { _progressBar :: Picture
     , _progressLoc :: Picture
+    , _speedometer :: Picture
+    , _arrow :: Picture
     }
 
 makeLenses ''Hud
 
 create :: IO Hud
-create = Hud <$> imgToPic 1 "assets/progress_bar.png" <*> imgToPic 1 "assets/progress_loc.png"
+create = Hud <$> imgToPic 1 "assets/progress_bar.png"
+             <*> imgToPic 1 "assets/progress_loc.png"
+             <*> imgToPic 0.6 "assets/speedometer.png"
+             <*> imgToPic 0.6 "assets/arrow.png"
 
 render :: World -> Hud -> Picture
-render w h = renderProgress
+render w h = renderProgress <> renderSpeed
   where
     renderProgress = translate 0 300
                    $ h ^. progressBar
                   <> translate (1000 * (0.5 - curDist / initDist)) 0 (h ^. progressLoc)
     initDist = mag $ B.center (w ^. W.finish) - w ^. W.start
     curDist = mag $ B.center (w ^. W.finish) - w ^. W.player . P.position
+    renderSpeed = renderSpeedometer <> renderArrow
+    renderSpeedometer = translate (-500) (-300) $ h ^. speedometer
+    renderArrow
+        = translate (-500) (-330)
+        . rotate (mag (w ^. W.player . P.velocity) / 10 - 110)
+        . translate 0 30
+        $ h ^. arrow
