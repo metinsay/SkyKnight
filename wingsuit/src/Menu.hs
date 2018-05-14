7d5896749219628adb05bd232a81ccce25f096c7
tysonzero <danielspencersmith@gmail.com>
2018-05-14 11:38:22 -0400

    Improve main menu

--- wingsuit/src/Menu.hs
+++ wingsuit/src/Menu.hs
@@ -18,29 +18,58 @@ import Levels (levels)
 import Scores (Scores, getScore)
 
 data Menu = Menu
-    { _image :: Picture
+    { _location :: Location
+    , _image :: Picture
     }
+
+data Location = Home | Play
 
 makeLenses ''Menu
 
 create :: IO Menu
-create = Menu <$> imgToPic 0.75 "assets/menu.png"
+create = Menu Home <$> imgToPic 0.75 "assets/menu.png"
 
 render :: Scores -> Menu -> Picture
-render ss m = m ^. image
-           <> foldMap B.render ((^. _1) <$> levelButtons ss)
+render ss m = m ^. image <> case m ^. location of
+    Home -> renderHome
+    Play -> renderPlay ss
 
-handle :: Event -> Scores -> Maybe (Maybe (String, Level))
-handle (EventKey (SpecialKey KeyEsc) Down _ _) _ = Just Nothing
-handle e ss = go $ levelButtons ss
+renderHome :: Picture
+renderHome = foldMap B.render (fst <$> homeButtons)
+
+renderPlay :: Scores -> Picture
+renderPlay ss = foldMap B.render ((^. _1) <$> playButtons ss)
+
+handle :: Event -> Scores -> Menu -> Either (Maybe (String, Level)) Menu
+handle e ss m = case m ^. location of
+    Home -> maybe (Left Nothing) Right (handleHome e m)
+    Play -> either (Left . Just) Right (handlePlay e ss m)
+
+handleHome :: Event -> Menu -> Maybe Menu
+handleHome (EventKey (SpecialKey KeyEsc) Down _ _) _ = Nothing
+handleHome e m = case filter (B.handle e . fst) homeButtons of
+    [] -> Just m
+    ((_, ml) : _) -> maybe Nothing (\l -> Just $ m & location .~ l) ml
+
+handlePlay :: Event -> Scores -> Menu -> Either (String, Level) Menu
+handlePlay (EventKey (SpecialKey KeyEsc) Down _ _) _ m = Right $ m & location .~ Home
+handlePlay e ss m = go $ playButtons ss
   where
-    go [] = Nothing
+    go [] = case e of
+        EventKey (MouseButton LeftButton) Down _ _ -> Right $ m & location .~ Home
+        _ -> Right m
     go ((b, n, l) : bs)
-        | B.handle e b = Just (Just (n, l))
+        | B.handle e b = Left (n, l)
         | otherwise = go bs
 
-levelButtons :: Scores -> [(Button, String, Level)]
-levelButtons ss = zipWith mkLevelButton [-offset .. offset] (M.toList levels)
+homeButtons :: [(Button, Maybe Location)]
+homeButtons =
+    [ (Button "Play" (-200, 25) (200, 75), Just Play)
+    , (Button "Quit (Esc)" (-200, -75) (200, -25), Nothing)
+    ]
+
+playButtons :: Scores -> [(Button, String, Level)]
+playButtons ss = zipWith mkLevelButton [-offset .. offset] (M.toList levels)
   where
     mkLevelButton i (n, l) = case getScore n ss of
         Nothing -> ( Button n (-200, 100 * i - 25) (200, 100 * i + 25), n, l )
