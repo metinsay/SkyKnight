a90ded97354250af2f5d8cf278840146312906fb
tysonzero <danielspencersmith@gmail.com>
2018-05-14 14:15:43 -0400

    More improvements to finish screen

--- wingsuit/src/FinishHud.hs
+++ wingsuit/src/FinishHud.hs
@@ -18,14 +18,14 @@ render score w = wonText <> timeText <> acornText <> scoreText
               . color (makeColor 1 1 0 1)
               . translate (-500) 100
               . text
-              $ "Time: " ++ showFFloat (Just 2) (w ^. time) "s"
+              $ "Time left: " ++ showFFloat (Just 2) (w ^. time) "s"
 
     acornText = fuzz
               . scale 0.4 0.4
               . color (makeColor 1 1 0 1)
               . translate (-500) (-200)
               . text
-              $ "Acorns: 3 * " ++ showFFloat (Just 0) (acornCount w) ""
+              $ "Acorns: " ++ showFFloat (Just 0) (acornCount w) " * 3"
 
     scoreText = fuzz
               . scale 0.4 0.4
