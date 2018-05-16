595df022ad5cc904d77fdb1c5e7a9bfcb316d1e4
tysonzero <danielspencersmith@gmail.com>
2018-05-16 19:41:39 -0400

    Port Audio to only use sdl2

--- wingsuit/src/Audio.hs
+++ wingsuit/src/Audio.hs
@@ -2,13 +2,14 @@
 
 module Audio (Audio, initialize, setBackground, playSound, menuMusic, airLoop) where
 
-import Base
 import qualified SDL
-import qualified Graphics.UI.SDL.Mixer as Mix
-import Foreign
+import qualified SDL.Mixer as Mix
 import Control.Concurrent
 import Control.Concurrent.STM.TChan (TChan, tryReadTChan, newTChanIO, writeTChan)
 import Control.Monad.STM (atomically)
+import Data.Default.Class (def)
+
+import Base
 
 data Audio = Audio
     { _musicCh :: TChan FilePath
@@ -30,10 +31,7 @@ initialize = do
     sound <- newTChanIO
     atomically $ writeTChan bgSound menuMusic
     SDL.initialize [SDL.InitAudio]
-    _ <- Mix.openAudio 44100 Mix.AudioS16LSB 2 4096
-    _ <- Mix.allocateChannels 3
-    _ <- Mix.volume 1 32 -- background volume
-    _ <- Mix.volume 2 128 -- sound volume
+    _ <- Mix.openAudio def 256
     _ <- forkOS $ playSoundHelper sound bgSound
     pure $ Audio sound bgSound menuMusic
 
@@ -42,9 +40,6 @@ setBackground :: Audio -> String -> IO Audio
 setBackground a s = if s == (a ^. curr)
                     then pure a
                     else do
-                        _ <- if s == airLoop
-                             then Mix.volume 1 128
-                             else Mix.volume 1 32
                         atomically $ writeTChan (a ^. bgMusicCh) s
                         pure $ a & curr .~ s
 
@@ -52,30 +47,26 @@ playSound :: Audio -> String -> IO ()
 playSound a s = atomically $ writeTChan (a ^. musicCh) s
 
 playSoundHelper :: TChan FilePath -> TChan FilePath -> IO ()
-playSoundHelper soundCh backgroundCh = do
-    keepAlive soundCh backgroundCh Nothing Nothing
+playSoundHelper soundCh backgroundCh = keepAlive soundCh backgroundCh Nothing Nothing
 
-keepAlive :: TChan FilePath -> TChan FilePath -> Maybe Mix.Chunk -> Maybe Mix.Chunk -> IO()
+keepAlive :: TChan FilePath -> TChan FilePath -> Maybe Mix.Chunk -> Maybe Mix.Chunk -> IO ()
 keepAlive soundCh backgroundCh soundToPlay backgroundToPlay = do
-    _ <- case soundToPlay of
-       Just s -> touchForeignPtr s
-       Nothing -> return ()
-    case backgroundToPlay of
-       Just s -> touchForeignPtr s
-       Nothing -> return ()
     threadDelay 50000
-
     newSound <- atomically $ tryReadTChan soundCh
     case newSound of
        Just p -> do
-          newPlay <- Mix.loadWAV p
-          _ <- Mix.playChannel 2 newPlay 0
+          newPlay <- Mix.load p
+          Mix.setVolume 128 newPlay -- sound volume
+          _ <- Mix.playOn 2 Mix.Once newPlay
           keepAlive soundCh backgroundCh (Just newPlay) backgroundToPlay
        Nothing -> do
            newBgSound <- atomically $ tryReadTChan backgroundCh
            case newBgSound of
               Just b -> do
-                 newPlay <- Mix.loadWAV b
-                 _ <- Mix.playChannel 1 newPlay (-1)
+                 newPlay <- Mix.load b
+                 if b == airLoop
+                     then Mix.setVolume 128 newPlay
+                     else Mix.setVolume 32 newPlay
+                 _ <- Mix.playOn 1 Mix.Forever newPlay
                  keepAlive soundCh backgroundCh soundToPlay (Just newPlay)
               Nothing -> keepAlive soundCh backgroundCh soundToPlay backgroundToPlay
